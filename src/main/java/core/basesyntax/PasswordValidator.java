6bd23104662bd7d8db7e24c9421dcb9daabc5507
noleynik29 <noleynik29@gmail.com>
2025-09-08 13:15:25 +0300

    Implemented registerUser() and validate() functions for PasswordValidator and UserService. Also implemented PasswordValidationException.

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,6 +4,9 @@ public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         //write your code here
+        if (password == null && repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
         if (password.length() >= 10 && repeatPassword.length() >= 10
                 && repeatPassword.equals(password)) {
             System.out.println("Password matches");
